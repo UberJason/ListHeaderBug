4ad11fc8717ddcb88146bd71cc227c71001258c3
Jason Ji <uberjason@gmail.com>
2020-06-24 22:54:15 -0400

    Initial Commit

---
+++ ListHeaderBug/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  ListHeaderBug
+//
+//  Created by Jason Ji on 6/24/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
